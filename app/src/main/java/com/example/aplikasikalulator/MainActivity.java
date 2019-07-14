5cdd1dd478cc58db7998f6c7a3087e2f6fba43b0
Eduard Wade Lerman <eduardsnaga@gmail.com>
2019-07-14 21:47:13 +0700

    Penambahan function yang lain

--- app/src/main/java/com/example/aplikasikalulator/MainActivity.java
+++ app/src/main/java/com/example/aplikasikalulator/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.aplikasikalulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         bagi = (Button) findViewById(R.id.dividebtn);
         bersihkan = (Button) findViewById(R.id.clr);
 
+
         tambah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,6 +49,65 @@ public class MainActivity extends AppCompatActivity {
                     Toast toast = Toast.makeText(MainActivity.this, "Mohon Masukkan angka Pertama dan Kedua", Toast.LENGTH_LONG);
                     toast.show();
                 }
+            }
+        });
+
+        kurang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((angka_pertama.getText().length()>0) && (angka_kedua.getText().length()>0))
+                {
+                    double angkainp1 = Double.parseDouble(angka_pertama.getText().toString());
+                    double angkainp2 = Double.parseDouble(angka_kedua.getText().toString());
+                    double result = angkainp1-angkainp2;
+                    jumlah.setText(Double.toString(result));
+                }
+                else {
+                    Toast toast = Toast.makeText(MainActivity.this, "Mohon Masukkan angka Pertama dan Kedua", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+        kali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((angka_pertama.getText().length()>0) && (angka_kedua.getText().length()>0))
+                {
+                    double angkainp1 = Double.parseDouble(angka_pertama.getText().toString());
+                    double angkainp2 = Double.parseDouble(angka_kedua.getText().toString());
+                    double result = angkainp1*angkainp2;
+                    jumlah.setText(Double.toString(result));
+                }
+                else {
+                    Toast toast = Toast.makeText(MainActivity.this, "Mohon Masukkan angka Pertama dan Kedua", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+        bagi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((angka_pertama.getText().length()>0) && (angka_kedua.getText().length()>0))
+                {
+                    double angkainp1 = Double.parseDouble(angka_pertama.getText().toString());
+                    double angkainp2 = Double.parseDouble(angka_kedua.getText().toString());
+                    double result = angkainp1/angkainp2;
+                    jumlah.setText(Double.toString(result));
+                }
+                else {
+                    Toast toast = Toast.makeText(MainActivity.this, "Mohon Masukkan angka Pertama dan Kedua", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
+        bersihkan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angka_pertama.setText("");
+                angka_kedua.setText("");
+                jumlah.setText("");
+                angka_pertama.requestFocus();
             }
         });
     }
